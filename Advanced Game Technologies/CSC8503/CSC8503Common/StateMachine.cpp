f194972b5e68f807d56749aa928655191cf99d45
Bailey <baileygardner25@gmail.com>
2019-12-03 20:31:07 +0000

    state machine tutorial code completed

--- Advanced Game Technologies/CSC8503/CSC8503Common/StateMachine.cpp
+++ Advanced Game Technologies/CSC8503/CSC8503Common/StateMachine.cpp
@@ -15,13 +15,28 @@ StateMachine::~StateMachine()
 }
 
 void StateMachine::AddState(State* s) {
-
+	allStates.emplace_back(s);
+	if (activeState == nullptr)
+		activeState = s;
 }
 
 void StateMachine::AddTransition(StateTransition* t) {
-
+	allTransitions.insert(std::make_pair(t->GetSourceState(), t));
 }
 
 void StateMachine::Update() {
+	if (activeState) {
+		activeState->Update();
 
+		// get transition set starting from this state node
+		std::pair<TransitionIterator, TransitionIterator> range = allTransitions.equal_range(activeState);
+
+		// iterate through them all
+		for (auto& i = range.first; i != range.second; ++i) {
+			if (i->second->CanTransition()) {
+				State* newState = i->second->GetDestinationState();
+				activeState = newState;
+			}
+		}
+	}
 }
--- Advanced Game Technologies/CSC8503/GameTech/Main.cpp
+++ Advanced Game Technologies/CSC8503/GameTech/Main.cpp
@@ -16,6 +16,45 @@ using namespace NCL;
 using namespace CSC8503;
 
 void TestStateMachine() {
+	StateMachine* testMachine = new StateMachine();
+
+	int someData = 0;
+
+	StateFunc AFunc = [](void* data) {
+		int* realData = (int*)data;
+		(*realData)++;
+		std::cout << "In State A" << std::endl;
+	};
+
+	StateFunc BFunc = [](void* data) {
+		int* realData = (int*)data;
+		(*realData)--;
+		std::cout << "In State B" << std::endl;
+	};
+
+	GenericState* stateA = new GenericState(AFunc, (void*)&someData);
+	GenericState* stateB = new GenericState(BFunc, (void*)&someData);
+	testMachine->AddState(stateA);
+	testMachine->AddState(stateB);
+
+	// if greater than 10, A to B
+	GenericTransition<int&, int>* transitionA = new GenericTransition<int&, int>(
+														GenericTransition<int&, int>::GreaterThanTransition, 
+														someData, 10, stateA, stateB);
+
+	// if equals 0, B to A
+	GenericTransition<int&, int>* transitionB = new GenericTransition<int&, int>(
+		GenericTransition<int&, int>::EqualsTransition,
+		someData, 0, stateB, stateA);
+
+	testMachine->AddTransition(transitionA);
+	testMachine->AddTransition(transitionB);
+
+	for (int i = 0; i < 100; ++i) {
+		// run state machine
+		testMachine->Update();
+	}
+	delete testMachine;
 }
 
 void TestNetworking() {
@@ -52,7 +91,7 @@ int main() {
 		return -1;
 	}	
 
-	//TestStateMachine();
+	TestStateMachine();
 	//TestNetworking();
 	//TestPathfinding();
 	
