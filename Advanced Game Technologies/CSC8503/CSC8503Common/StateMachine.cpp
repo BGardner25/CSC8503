4a10565e982f65dd2daa08f5b3f321e494e75e03
Bailey <baileygardner25@gmail.com>
2019-12-05 17:48:17 +0000

    fixes

--- Advanced Game Technologies/CSC8503/CSC8503Common/StateMachine.cpp
+++ Advanced Game Technologies/CSC8503/CSC8503Common/StateMachine.cpp
@@ -4,24 +4,37 @@
 
 using namespace NCL::CSC8503;
 
-StateMachine::StateMachine()
-{
+StateMachine::StateMachine() {
 	activeState = nullptr;
 }
 
-StateMachine::~StateMachine()
-{
+StateMachine::~StateMachine() {
 
 }
 
 void StateMachine::AddState(State* s) {
-
+	allStates.emplace_back(s);
+	if (activeState == nullptr)
+		activeState = s;
 }
 
 void StateMachine::AddTransition(StateTransition* t) {
-
+	allTransitions.insert(std::make_pair(t->GetSourceState(), t));
 }
 
 void StateMachine::Update() {
+	if (activeState) {
+		activeState->Update();
 
+		// get transition set starting from this state node
+		std::pair<TransitionIterator, TransitionIterator> range = allTransitions.equal_range(activeState);
+
+		// iterate through them all
+		for (auto& i = range.first; i != range.second; ++i) {
+			if (i->second->CanTransition()) {
+				State* newState = i->second->GetDestinationState();
+				activeState = newState;
+			}
+		}
+	}
 }
