495ee75d3d72cf1ebd8cabcd66c967efb33958e5
b6033760 <b6033760@18SCOMPD158>
2019-12-06 12:35:53 +0000

    broadphase started

--- Advanced Game Technologies/CSC8503/CSC8503Common/QuadTree.h
+++ Advanced Game Technologies/CSC8503/CSC8503Common/QuadTree.h
@@ -43,15 +43,51 @@ namespace NCL {
 			}
 
 			void Insert(T& object, const Vector3& objectPos, const Vector3& objectSize, int depthLeft, int maxSize) {
+				if (!CollisionDetection::AABBTest(objectPos, Vector3(position.x, 0, position.y), objectSize, Vector3(size.x, 1000.0f, size.y)))
+					return;
+				// not a leaf node so descend down tree
+				if (children)
+					for (int i = 0; i < 4; ++i)
+						children[i].Insert(object, objectPos, objectSize, depthLeft - 1, maxSize);
+				else {	// leaf node so expand
+					contents.push_back(QuadTreeEntry<T>(object, objectPos, objectSize));
+					if ((int)contents.size() > maxSize && depthLeft > 0) {
+						if (!children) {
+							Split();
+							// reinsert contents
+							for (const auto& i : contents) {
+								for (int j = 0; j < 4; ++j) {
+									auto entry = i;
+									children[j].Insert(entry.object, entry.pos, entry.size, depthLeft - 1, maxSize);
+								}
+							}
+							contents.clear();
+						}
+					}
+				}
 			}
 
+			// split node into 4 child nodes that fill the same area of original node
 			void Split() {
+				Vector2 halfSize = size / 2.0f;		// * 0.5f??
+				children = new QuadTreeNode<T>[4];
+				children[0] = QuadTreeNode<T>(position + Vector2(-halfSize.x, halfSize.y), halfSize);
+				children[1] = QuadTreeNode<T>(position + Vector2(halfSize.x, halfSize.y), halfSize);
+				children[2] = QuadTreeNode<T>(position + Vector2(-halfSize.x, -halfSize.y), halfSize);
+				children[3] = QuadTreeNode<T>(position + Vector2(halfSize.x, -halfSize.y), halfSize);
 			}
 
 			void DebugDraw() {
 			}
 
 			void OperateOnContents(QuadTreeFunc& func) {
+				// check node has children and recursively call until we reach a leaf node i.e. no children
+				if (children)
+					for (int i = 0; i < 4; ++i)
+						children[i].OperateOnContents(func);
+				else
+					if (!contents.empty())
+						func(contents);
 			}
 
 		protected:
